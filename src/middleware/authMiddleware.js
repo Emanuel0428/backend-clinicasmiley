fbcc4cffb0e93f6111af77dc0d949229800ae21f
Emanuel0428 <cod2877@gmail.com>
2025-04-11 17:26:04 -0500

    Se elimina archivo no usado y se agregan validaciones al middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,23 +1,24 @@
-// src/middleware/authMiddleware.js
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1]; // Extract token from "Bearer <token>"
-
-  if (!token) {
-    const err = new Error('Token no proporcionado');
-    err.statusCode = 401;
-    throw err;
-  }
-
   try {
+    const authHeader = req.headers.authorization;
+    
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      const err = new Error('Token no proporcionado');
+      err.statusCode = 401;
+      return next(err);
+    }
+
+    const token = authHeader.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Attach the decoded user data to the request
+
+    req.user = decoded;
     next();
   } catch (err) {
     const error = new Error('Token inválido o expirado');
     error.statusCode = 401;
-    throw error;
+    return next(error); // Usa next para enviar el error al manejador
   }
 };
 
